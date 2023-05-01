90ef06796b86c84b67ef966e7752d8f1790c9537
eduardo morua <eduardo.r.morua@gmail.com>
2023-04-30 18:20:18 -0600

    adding product category interface

---
+++ src/interfaces/product-category.interface.ts
@@ -0,0 +1,10 @@
+import { CategoryI } from './category.interface';
+import { ProductI } from './product.interface';
+
+export interface ProductCategoryI {
+	id: number;
+	categoryId: number;
+	productId: number;
+	product: ProductI;
+	category: CategoryI;
+}
